98a1562df776c7d89723deb9b6caab759420e769
SriSathis <srisathishmsc@gmail.com>
2019-11-28 15:40:18 +0530

    Mysql env variable added

--- mysql/mysql-connector.py
+++ mysql/mysql-connector.py
@@ -1,12 +1,15 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
 
 class Mysql:
+    load_dotenv(dotenv_path='../.env')
     def Mysql_connection():
         mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            passwd="Sathish-12#",
-            database="mysql"
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_USER'),
+            passwd=os.getenv('MYSQL_PASSWORD'),
+            database=os.getenv('MYSQL_DB')
         )
 
         mycluster = mydb.cursor()
