8480d501040eeb03a449a37b10d13ecfe542e57d
SriSathis <srisathishmsc@gmail.com>
2019-11-28 19:06:05 +0530

    class removed

--- mysql/mysql-connector.py
+++ mysql/mysql-connector.py
@@ -2,21 +2,21 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
-class Mysql:
-    load_dotenv(dotenv_path='../.env')
-    def Mysql_connection():
-        mydb = mysql.connector.connect(
-            host=os.getenv('MYSQL_HOST'),
-            user=os.getenv('MYSQL_USER'),
-            passwd=os.getenv('MYSQL_PASSWORD'),
-            database=os.getenv('MYSQL_DB')
-        )
+    
+def Mysql_connection():
+    mydb = mysql.connector.connect(
+        host=os.getenv('MYSQL_HOST'),
+        user=os.getenv('MYSQL_USER'),
+        passwd=os.getenv('MYSQL_PASSWORD'),
+        database=os.getenv('MYSQL_DB')
+    )
 
-        mycluster = mydb.cursor()
+    mycluster = mydb.cursor()
 
-        mycluster.execute('SELECT * FROM db')
-        for i in mycluster:
-            print(i)
+    mycluster.execute('SELECT * FROM db')
+    for i in mycluster:
+        print(i)
 
 if __name__ == "__main__":
-    Mysql.Mysql_connection()
+    load_dotenv(dotenv_path='../.env')
+    Mysql_connection()
