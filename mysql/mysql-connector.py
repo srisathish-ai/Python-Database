6d7a06e70f4fa2b538894fc8b1d7c76a5e2d5e27
SriSathis <srisathishmsc@gmail.com>
2019-11-29 12:38:55 +0530

    MySQL try block added

--- mysql/mysql-connector.py
+++ mysql/mysql-connector.py
@@ -4,18 +4,25 @@ from dotenv import load_dotenv
 
     
 def Mysql_connection():
-    mydb = mysql.connector.connect(
-        host=os.getenv('MYSQL_HOST'),
-        user=os.getenv('MYSQL_USER'),
-        passwd=os.getenv('MYSQL_PASSWORD'),
-        database=os.getenv('MYSQL_DB')
-    )
+    try:
+        mydb = mysql.connector.connect(
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_USER'),
+            passwd=os.getenv('MYSQL_PASSWORD'),
+            database=os.getenv('MYSQL_DB')
+        )
 
-    mycluster = mydb.cursor()
-
-    mycluster.execute('SELECT * FROM db')
-    for i in mycluster:
-        print(i)
+        mycluster = mydb.cursor()
+        print("Mysql Connection")
+        mycluster.execute('SELECT * FROM db')
+        for i in mycluster:
+            print(i)
+    except mysql.connector.DatabaseError as error:
+        print("There is a problem with mysql connection \n",error)
+    finally:
+        if mycluster is not None:
+            mycluster.close()
+            print("The MySQL connection is Closed")
 
 if __name__ == "__main__":
     load_dotenv(dotenv_path='../.env')
