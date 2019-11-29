646ab1d9f063e21b01289846268de61e9991dbd6
SriSathis <srisathishmsc@gmail.com>
2019-11-29 12:33:29 +0530

    Added Try and catch block

--- postgres/postgres.py
+++ postgres/postgres.py
@@ -5,22 +5,26 @@ from dotenv import load_dotenv
 
 def postgres_connection():
     load_dotenv(dotenv_path='../.env')
-    conn = psycopg2.connect(
-        host=os.getenv('PG_HOST'),
-        port=os.getenv('PG_PORT'),
-        user=os.getenv('PG_USER'),
-        password=os.getenv('PG_PASSWORD'),
-        database=os.getenv('PG_DB')
-    )
-    
-    mycluster = conn.cursor()
-    print('Postgres connection')
-    mycluster.execute('SELECT * FROM public.role')
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv('PG_HOST'),
+            port=os.getenv('PG_PORT'),
+            user=os.getenv('PG_USER'),
+            password=os.getenv('PG_PASSWORD'),
+            database=os.getenv('PG_DB')
+        )
 
-    for i in mycluster:
-        print(i)
-
-    mycluster.close()
+        mycluster = conn.cursor()
+        print('Postgres connection')
+        mycluster.execute('SELECT * FROM public.role')
+        for i in mycluster:
+            print(i)
+    except psycopg2.DatabaseError as error:
+        print("There is a problem with postgres \n",error)
+    finally:
+        if mycluster is not None:
+            mycluster.close()
+            print("The Postgres connection is Closed")
 
 if __name__ == "__main__":
     postgres_connection()
